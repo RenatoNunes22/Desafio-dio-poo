9462f009983efb09f10c31bf1d8803e16fb244f1
RenatoNunes22 <renatonunes0011@hotmail.com>
2022-07-20 20:38:53 -0300

    Finalizando desafio bootcamp java

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -9,13 +9,41 @@ public class Main {
 
         curso.setTitulo("Curso Java");
         curso.setDescricao("Descrição do curso java");
-        curso.setCargaHoraria(50);
+        curso.setCargaHoraria(8);
 
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRenato = new Dev();
+        devRenato.setNome("Renato");
+        devRenato.inscreveBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos " + devRenato.getNome() + ": " + devRenato.getConteudoInscrito());
+        System.out.println("Conteudo Concluidos " + devRenato.getNome() + ": " + devRenato.getConteudoConcluido());
+        System.out.println("XP de " + devRenato.getNome() + ":" + devRenato.calcularTotalXp());
+        System.out.println("------------------------------------------------------");
+        devRenato.progredir();
+        devRenato.progredir();
+        System.out.println("Conteudo Inscritos " + devRenato.getNome() + ": " + devRenato.getConteudoInscrito());
+        System.out.println("Conteudo Concluidos " + devRenato.getNome() + ": " + devRenato.getConteudoConcluido());
+        System.out.println("XP de " + devRenato.getNome() + ":" + devRenato.calcularTotalXp());
+        Dev devAleatorio = new Dev();
+        devAleatorio.setNome("Gabriel");
+        System.out.println("**------------------------------------------------------**");
+        devAleatorio.inscreveBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos " + devAleatorio.getNome() + ": "+ devAleatorio.getConteudoInscrito());
+        System.out.println("Conteudo Concluidos " + devAleatorio.getNome() + ": " + devAleatorio.getConteudoConcluido());
+        System.out.println("XP de " + devAleatorio.getNome() + ":" + devAleatorio.calcularTotalXp());
+        System.out.println("-----------------------------------------------------");
+        devAleatorio.progredir();
+        System.out.println("Conteudo Inscritos " + devAleatorio.getNome() + ": "+ devAleatorio.getConteudoInscrito());
+        System.out.println("Conteudo Concluidos " + devAleatorio.getNome() + ": " + devAleatorio.getConteudoConcluido());
+        System.out.println("XP de " + devAleatorio.getNome() + ":" + devAleatorio.calcularTotalXp());
     }
 }
